bbf6b5b37ef772061ce81962d819fa3904efcc00
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-19 17:53:44 +0200

    Update action-LichtAN.py

--- action-LichtAN.py
+++ action-LichtAN.py
@@ -13,5 +13,5 @@ def intent_callback_LichtAN(hermes, intentMessage):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("studger:LichtAN", intent_callback_Tschuess)
+        h.subscribe_intent("studger:LichtAN", intent_callback_LichtAN)
         h.start()
