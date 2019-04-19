91b63ea9ef04e75efa918abf5be6173dbfea91cc
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-19 19:39:49 +0200

    Update action-LichtAN.py

--- action-LichtAN.py
+++ action-LichtAN.py
@@ -6,18 +6,29 @@ from hermes_python.ontology import *
 import io
 
 SITE = "default"
-SOUNDFILE = "./LichtAN.wav"
-binaryFile = open(SOUNDFILE, mode='rb')
-wav = bytearray(binaryFile.read())
 
 def intent_callback_LichtAN(hermes, intentMessage):
+    SITE = "default"
+    SOUNDFILE = "./LichtAN.wav"
+    binaryFile = open(SOUNDFILE, mode='rb')
+    wav = bytearray(binaryFile.read())
+    result_sentence = "OK. Licht ist eingeschaltet."
+    current_session_id = intentMessage.session_id
+    publish.single("hermes/audioServer/{}/playBytes/whateverId".format(SITE),payload=wav, hostname="localhost", client_id="")
+    #hermes.publish_end_session(current_session_id, result_sentence)    
+    
+def intent_callback_LichtAUS(hermes, intentMessage):
+    SITE = "default"
+    SOUNDFILE = "./LichtAus.wav"
+    binaryFile = open(SOUNDFILE, mode='rb')
+    wav = bytearray(binaryFile.read())
     result_sentence = "OK. Licht ist eingeschaltet."
     current_session_id = intentMessage.session_id
     publish.single("hermes/audioServer/{}/playBytes/whateverId".format(SITE),payload=wav, hostname="localhost", client_id="")
     #hermes.publish_end_session(current_session_id, result_sentence)    
 
-
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("studger:LichtAN", intent_callback_LichtAN)
+        h.subscribe_intent("studger:LichtAN", intent_callback_LichtAus)
         h.start()
