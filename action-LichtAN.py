396c51ee5847cb63808ccc96a7f9a5ef2d3a49df
jonniki <46942739+jonniki@users.noreply.github.com>
2019-04-19 18:45:53 +0200

    Update action-LichtAN.py

--- action-LichtAN.py
+++ action-LichtAN.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import paho.mqtt.publish as publish
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 
+SITE = "default"
+SOUNDFILE = "./LichtAN.wav"
+binaryFile = open(SOUNDFILE, mode='rb')
+wav = bytearray(binaryFile.read())
+
 def intent_callback_LichtAN(hermes, intentMessage):
     result_sentence = "OK. Licht ist eingeschaltet."
     current_session_id = intentMessage.session_id
+    publish.single("hermes/audioServer/{}/playBytes/whateverId".format(SITE),payload=wav, hostname="localhost", client_id="")
     hermes.publish_end_session(current_session_id, result_sentence)    
 
 
